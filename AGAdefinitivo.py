2f164461dfb1491ee62af8c3c2bd961f068ef5e3
lucabpo2005 <lucabpo2005@gmail.com>
2026-06-23 16:21:32 -0300

    Update AGAdefinitivo.py

--- AGAdefinitivo.py
+++ AGAdefinitivo.py
@@ -222,10 +222,10 @@ if PDF_DISPONIBLE:
 
         # CORREGIDO: Anchos de columna explícitos asignados en puntos para ReportLab
         t_maestra = Table(data_reportlab, colWidths=[120, 75, 75, 75, 125, 100])
-        t_maestra.setStyle(TableStyle([
+        t_maestra.setStyle(TableStyle)[
             ('BACKGROUND', (0,0), (-1,0), colors.HexColor("#2B6CB0")),
             ('TEXTCOLOR', (0,0), (-1,0), colors.whitesmoke),
             ('ALIGN', (0,0), (-1,-1), 'LEFT'),
             ('VALIGN', (0,0), (-1,-1), 'MIDDLE'),
             ('BOTTOMPADDING', (0,0), (-1,-1), 5),
-])
+]
